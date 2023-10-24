67144c1229ee00671f3648ac8c34471a2b81d727
Xameon <andrej.shmalenko@gmail.com>
2023-10-24 19:37:25 +0300

    Setup Google Authentication and partition setup auth with Email and Password.

--- src/components/sign-in-form/sign-in-form.component.jsx
+++ src/components/sign-in-form/sign-in-form.component.jsx
@@ -1,23 +1,87 @@
-import './sign-in-form.styles.scss';
+import { useGoogleLogin } from '@react-oauth/google';
 import FormInput from '../form-input/form-input.component';
+import './sign-in-form.styles.scss';
+import { useState, useEffect } from 'react';
+
+const defaultFormFields = { email: '', password: '' };
 
 const SignInForm = () => {
+  const [googleUser, setGoogleUser] = useState(null);
+  const [formFields, setFormFields] = useState(defaultFormFields);
+  const { email, password } = formFields;
+
+  useEffect(() => {
+    if (googleUser) {
+      const { access_token } = googleUser;
+      const passAccessToken = async () => {
+        const response = await fetch(
+          'http://pc-api-env.eba-ff8mdmg7.us-east-1.elasticbeanstalk.com/api/auth/google',
+          {
+            headers: {
+              authorization: access_token,
+            },
+          }
+        );
+        const { accessToken, refreshToken } = await response.json();
+      };
+
+      passAccessToken();
+    }
+  }, [googleUser]);
+
+  const loginWithGoogle = useGoogleLogin({
+    onSuccess: (codeResponse) => setGoogleUser(codeResponse),
+    onError: (error) => console.log('Login Failed:', error),
+  });
+
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+    setFormFields({ ...formFields, [name]: value });
+  };
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+
+    try {
+      const response = await fetch(
+        'http://pc-api-env.eba-ff8mdmg7.us-east-1.elasticbeanstalk.com/api/auth/login',
+        {
+          method: 'POST',
+          headers: {
+            'content-type': 'application/json',
+          },
+          body: JSON.stringify({ email, password }),
+        }
+      );
+      const data = await response.json();
+      console.log(data);
+    } catch {}
+  };
+
   return (
     <div className='sign-in-container'>
       <h2>Already have an account?</h2>
       <span>Sign in with your email and password</span>
-      <form>
+      <form onSubmit={(event) => handleSubmit(event)}>
         <FormInput
-          label='Display Name'
-          type='text'
+          label='Email'
+          type='email'
           required
-          name='displayName'
+          name='email'
+          onChange={(e) => handleChange(e)}
         />
-        <FormInput label='Email' type='email' required name='email' />
-        <FormInput label='Password' type='password' required name='password' />
+        <FormInput
+          label='Password'
+          type='password'
+          required
+          name='password'
+          onChange={(e) => handleChange(e)}
+        />
         <div className='buttons-container'>
           <button type='submit'>Sign In</button>
-          <button type='button'>Sign In with Google</button>
+          <button type='button' onClick={() => loginWithGoogle()}>
+            Sign In With Google
+          </button>
         </div>
       </form>
     </div>
--- src/index.js
+++ src/index.js
@@ -1,17 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
+import { GoogleOAuthProvider } from '@react-oauth/google';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import './index.scss';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </React.StrictMode>
+  <GoogleOAuthProvider clientId='205106832327-2tmjsfmgk023j0r9i84p4n6tuaq5dths.apps.googleusercontent.com'>
+    <React.StrictMode>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </React.StrictMode>
+  </GoogleOAuthProvider>
 );
 
 // If you want to start measuring performance in your app, pass a function
